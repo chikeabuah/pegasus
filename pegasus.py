a442af38d53a415bb640f961a99ff8bbb4ae1930
Joe Near <jnear@uvm.edu>
2019-05-09 11:33:19 -0400

    working together on abovethreshold

--- pegasus.py
+++ pegasus.py
@@ -35,10 +35,10 @@ def smooth(cs,ps):
     for i in range(len(cs)):
       p = ps[i]
       g = p[-1]
-      newcs.append(stats.median(index(cs,g)))
+      newcs.append(np.median(index(cs,g)))
     return newcs
 
-def group(counts,eps,thresh):
+def old_group(counts,eps,thresh):
   final_output = []
   output = []
   thresh1 = thresh
@@ -66,3 +66,60 @@ def group(counts,eps,thresh):
     print(f'partition at time {i} is {output}')
     final_output.append(copy.deepcopy(output))
   return final_output
+
+def AboveThreshold(queries, epsilon, threshold):
+    T_hat = lap(threshold, 2/epsilon)
+
+    for i, q in enumerate(queries):
+      q_hat = lap(q, 4/epsilon)
+
+      if q_hat > T_hat:
+        return i
+
+    return None
+
+
+# this is epsilon-DP
+def group(counts,eps,thresh):
+  idx = 0
+  groups = []
+  while (idx < len(counts)):
+    #print(f'slice is {counts[idx:idx]}')
+    queries = [dev(counts[idx:p]) for p in range(idx+1, len(counts))]
+    #print(f'Queries is: {queries}')
+    newIdx = AboveThreshold(queries, eps, thresh)
+    if (newIdx is None):
+      newIdx = len(counts)
+    else:
+      newIdx = newIdx + 1
+    #print(f'newIdx is: {newIdx}')
+    #groups.append(counts[idx:newIdx])
+    groups.append(list(range(idx,newIdx)))
+    idx = newIdx
+
+  return groups
+
+# this is just postprocessing
+def group_postprocess(groups, length):
+  output = []
+  for t in range(length):
+    myOutput = []
+    for g in groups:
+      if t in g:
+        myOutput.append(g[:g.index(t)+1])
+        break
+      else:
+        myOutput.append(g)
+    output.append(myOutput)
+  return output
+
+print(f'ex is {ex}')
+print(f'Group #1 (new): {group(ex, 1.0, 2)}')
+print(f'Group #2 (new): {group(ex2, 1.0, 2)}')
+print(f'Group #1 (post): {group_postprocess(group(ex, 1.0, 2), len(ex))}')
+print(f'Group #2 (post): {group_postprocess(group(ex2, 1.0, 2), len(ex))}')
+print(f'Group #1 (old): {old_group(ex, 1.0, 2)}')
+print(f'Group #2 (old): {old_group(ex2, 1.0, 2)}')
+print(ex)
+print(perturb(ex))
+print(smooth(perturb(ex), group_postprocess(group(ex, 1.0, 2), len(ex))))
